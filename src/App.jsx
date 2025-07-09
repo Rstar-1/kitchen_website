04c6e52fa832d2d33287d1816248aacc306e5f91
Barasingha999 <barasingha.877@gmail.com>
2025-07-09 21:30:27 +0530

    ganpati bapa morya

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Suspense, lazy } from "react";
+import { Routes, Route } from "react-router-dom";
+
+const Home = lazy(() => import("./pages/home/Home"));
+
+function App() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Suspense>
+  );
+}
+
+export default App;
