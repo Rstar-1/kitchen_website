833a4499132473877fb1aa5d52941f2fbf3f18d6
Rstar-1 <124575584+Rstar-1@users.noreply.github.com>
2025-08-11 14:28:28 +0530

    Update Banner.jsx

--- src/pages/home/components/Banner.jsx
+++ src/pages/home/components/Banner.jsx
@@ -2,19 +2,22 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <div className="flex items-center w-full py-100">
+    <div className="flex items-center w-full">
       <div className="w-50">
         <h2 className="head-text text-forth">
-          We are here to serve you here to
+          We do not cook, we create your emotions!
         </h2>
-        <p className="para-text text-gray mt-10">
-          Lorem ipsum is a dummy or placeholder text commonly used in graphic
-          design, publishing, and web development. Lorem ipsum is a dummy or
-          placeholder text commonly used in graphic design, publishing, and web
-          development.
+        <p className="para-text text-gray w-80 mt-15">
+          Lorem ipsum is a dummy or placeholder text commonly used in graphic design, publishing, and web development.
         </p>
       </div>
-      <div className="w-50"></div>
+      <div className="w-50" style={{ background: "url(\"https://nunforest.com/koad-demo/upload/banners/ban3.jpg\")" }}>
+        <img
+          src="https://themewagon.github.io/chefs-kitchen-nextjs-free/images/hero/banner-image.png"
+          alt="banner-1"
+          className="w-full object-contain banner-img"
+        />
+      </div>
     </div>
   );
 };
