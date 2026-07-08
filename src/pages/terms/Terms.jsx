5e28efdf4f4d55d336421d7432d3737608339215
raj <rajshetye.5855@gmail.com>
2026-07-08 13:30:57 +0530

    new

--- src/pages/terms/Terms.jsx
+++ src/pages/terms/Terms.jsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Banner from "../../components/Banner";
 import Container from "../../utility/Container";
-import useFadeReveal from "../../utility/Fade";
 
 const Terms = () => {
-  const addToRef = useFadeReveal({ delay: 20, y: 30 });
-
   return (
     <>
       <Banner
@@ -15,45 +12,143 @@ const Terms = () => {
       />
       <Container version="v2" className="bg-white">
         <div className="w-full py-40 sm-py-30 text-dark">
-          <div className="w-80 md-w-90 sm-w-full" ref={addToRef}>
-            
-            <h3 className="title-text text-dark font-600 mb-10">1. Acceptance of Terms</h3>
+          <div className="w-80 md-w-90 sm-w-full">
+            <p className="para-text text-gray mb-26 font-600">
+              Welcome to Ashmita Vinyls!
+            </p>
             <p className="para-text text-gray mb-26">
-              By accessing and using this website, you accept and agree to be bound by the terms and provision of this agreement. In addition, when using these services, you shall be subject to any posted guidelines or rules applicable to such services. Any participation in this service will constitute acceptance of this agreement.
+              These terms and conditions outline the rules and regulations for the use of Ashmita Vinyls Private Limited's Website, located at ashmitavinyls.com.
+            </p>
+            <p className="para-text text-gray mb-26">
+              By accessing this website we assume you accept these terms and conditions. Do not continue to use Ashmita Vinyls if you do not agree to take all of the terms and conditions stated on this page.
+            </p>
+            <p className="para-text text-gray mb-26">
+              The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice and all Agreements: "Client", "You" and "Your" refers to you, the person log on this website and compliant to the Company’s terms and conditions. "The Company", "Ourselves", "We", "Our" and "Us", refers to our Company. "Party", "Parties", or "Us", refers to both the Client and ourselves. All terms refer to the offer, acceptance and consideration of payment necessary to undertake the process of our assistance to the Client in the most appropriate manner for the express purpose of meeting the Client’s needs in respect of provision of the Company’s stated services, in accordance with and subject to, prevailing law of Netherlands. Any use of the above terminology or other words in the singular, plural, capitalization and/or he/she or they, are taken as interchangeable and therefore as referring to the same.
             </p>
 
-            <h3 className="title-text text-dark font-600 mb-10">2. Products & Services</h3>
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">Cookies</h3>
             <p className="para-text text-gray mb-26">
-              Ashmita Vinyls Private Limited specializes in manufacturing and supplying premium PVC coated tarpaulins, transparent sheeting, table covers, strip curtains, synthetic leather, wall panels, and artificial grass flooring. All product specifications, availability, and pricing are subject to change without notice. We reserve the right to limit the quantity of products supplied to any customer.
+              We employ the use of cookies. By accessing Ashmita Vinyls, you agreed to use cookies in agreement with the Ashmita Vinyls Private Limited's Privacy Policy.
+            </p>
+            <p className="para-text text-gray mb-26">
+              Most interactive websites use cookies to let us retrieve the user’s details for each visit. Cookies are used by our website to enable the functionality of certain areas to make it easier for people visiting our website. Some of our affiliate/advertising partners may also use cookies.
             </p>
 
-            <h3 className="title-text text-dark font-600 mb-10">3. Orders & Payment</h3>
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">License</h3>
             <p className="para-text text-gray mb-26">
-              All orders are subject to acceptance by us. Payments must be made in full as per the invoice terms. We provide GST-compliant tax invoices for all corporate and bulk transactions. Any taxes, duties, or shipping fees will be calculated and specified in the invoice.
+              Unless otherwise stated, Ashmita Vinyls Private Limited and/or its licensors own the intellectual property rights for all material on Ashmita Vinyls. All intellectual property rights are reserved. You may access this from Ashmita Vinyls for your own personal use subjected to restrictions set in these terms and conditions.
+            </p>
+            <p className="para-text text-gray mb-10">You must not:</p>
+            <ul className="list-disc pl-20 mb-26">
+              <li className="para-text text-gray mb-8">Republish material from Ashmita Vinyls</li>
+              <li className="para-text text-gray mb-8">Sell, rent or sub-license material from Ashmita Vinyls</li>
+              <li className="para-text text-gray mb-8">Reproduce, duplicate or copy material from Ashmita Vinyls</li>
+              <li className="para-text text-gray mb-8">Redistribute content from Ashmita Vinyls</li>
+            </ul>
+            <p className="para-text text-gray mb-26">
+              This Agreement shall begin on the date hereof.
+            </p>
+            <p className="para-text text-gray mb-26">
+              Parts of this website offer an opportunity for users to post and exchange opinions and information in certain areas of the website. Ashmita Vinyls Private Limited does not filter, edit, publish or review Comments prior to their presence on the website. Comments do not reflect the views and opinions of Ashmita Vinyls Private Limited, its agents and/or affiliates. Comments reflect the views and opinions of the person who posts their views and opinions. To the extent permitted by applicable laws, Ashmita Vinyls Private Limited shall not be liable for the Comments or for any liability, damages or expenses caused and/or suffered as a result of any use of and/or posting of and/or appearance of the Comments on this website.
+            </p>
+            <p className="para-text text-gray mb-26">
+              Ashmita Vinyls Private Limited reserves the right to monitor all Comments and to remove any Comments which can be considered inappropriate, offensive or causes breach of these Terms and Conditions.
+            </p>
+            <p className="para-text text-gray mb-10">You warrant and represent that:</p>
+            <ul className="list-disc pl-20 mb-26">
+              <li className="para-text text-gray mb-8">You are entitled to post the Comments on our website and have all necessary licenses and consents to do so;</li>
+              <li className="para-text text-gray mb-8">The Comments do not invade any intellectual property right, including without limitation copyright, patent or trademark of any third party;</li>
+              <li className="para-text text-gray mb-8">The Comments do not contain any defamatory, libelous, offensive, indecent or otherwise unlawful material which is an invasion of privacy;</li>
+              <li className="para-text text-gray mb-8">The Comments will not be used to solicit or promote business or custom or present commercial activities or unlawful activity.</li>
+            </ul>
+            <p className="para-text text-gray mb-26">
+              You hereby grant Ashmita Vinyls Private Limited a non-exclusive license to use, reproduce, edit and authorize others to use, reproduce and edit any of your Comments in any and all forms, formats or media.
             </p>
 
-            <h3 className="title-text text-dark font-600 mb-10">4. Delivery & Shipping</h3>
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">Hyperlinking to our Content</h3>
+            <p className="para-text text-gray mb-10">The following organizations may link to our Website without prior written approval:</p>
+            <ul className="list-disc pl-20 mb-26">
+              <li className="para-text text-gray mb-8">Government agencies;</li>
+              <li className="para-text text-gray mb-8">Search engines;</li>
+              <li className="para-text text-gray mb-8">News organizations;</li>
+              <li className="para-text text-gray mb-8">Online directory distributors may link to our Website in the same manner as they hyperlink to the Websites of other listed businesses; and</li>
+              <li className="para-text text-gray mb-8">System wide Accredited Businesses except soliciting non-profit organizations, charity shopping malls, and charity fundraising groups which may not hyperlink to our Web site.</li>
+            </ul>
             <p className="para-text text-gray mb-26">
-              We ship our products across India and export to UAE, USA, and Bangladesh. While we strive to meet all delivery timelines, delivery schedules are estimates and depend on external logistics providers and customs clearance for international shipments.
+              These organizations may link to our home page, to publications or to other Website information so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products and/or services; and (c) fits within the context of the linking party’s site.
+            </p>
+            <p className="para-text text-gray mb-10">We may consider and approve other link requests from the following types of organizations:</p>
+            <ul className="list-disc pl-20 mb-26">
+              <li className="para-text text-gray mb-8">commonly-known consumer and/or business information sources;</li>
+              <li className="para-text text-gray mb-8">dot.com community sites;</li>
+              <li className="para-text text-gray mb-8">associations or other groups representing charities;</li>
+              <li className="para-text text-gray mb-8">online directory distributors;</li>
+              <li className="para-text text-gray mb-8">internet portals;</li>
+              <li className="para-text text-gray mb-8">accounting, law and consulting firms; and</li>
+              <li className="para-text text-gray mb-8">educational institutions and trade associations.</li>
+            </ul>
+            <p className="para-text text-gray mb-26">
+              We will approve link requests from these organizations if we decide that: (a) the link would not make us look unfavorably to ourselves or to our accredited businesses; (b) the organization does not have any negative records with us; (c) the benefit to us from the visibility of the hyperlink compensates the absence of Ashmita Vinyls Private Limited; and (d) the link is in the context of general resource information.
+            </p>
+            <p className="para-text text-gray mb-26">
+              These organizations may link to our home page so long as the link: (a) is not in any way deceptive; (b) does not falsely imply sponsorship, endorsement or approval of the linking party and its products or services; and (c) fits within the context of the linking party’s site.
+            </p>
+            <p className="para-text text-gray mb-26">
+              If you are one of the organizations listed in paragraph 2 above and are interested in linking to our website, you must inform us by sending an e-mail to Ashmita Vinyls Private Limited. Please include your name, your organization name, contact information as well as the URL of your site, a list of any URLs from which you intend to link to our Website, and a list of the URLs on our site to which you would like to link. Please allow 2-3 weeks for a response.
+            </p>
+            <p className="para-text text-gray mb-10">Approved organizations may hyperlink to our Website as follows:</p>
+            <ul className="list-disc pl-20 mb-26">
+              <li className="para-text text-gray mb-8">By use of our corporate name; or</li>
+              <li className="para-text text-gray mb-8">By use of the uniform resource locator being linked to; or</li>
+              <li className="para-text text-gray mb-8">By use of any other description of our Website being linked to that makes sense within the context and format of content on the linking party’s site.</li>
+            </ul>
+            <p className="para-text text-gray mb-26">
+              No use of Ashmita Vinyls Private Limited's logo or other artwork will be allowed for linking absent a trademark license agreement.
             </p>
 
-            <h3 className="title-text text-dark font-600 mb-10">5. Limitation of Liability</h3>
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">iFrames</h3>
             <p className="para-text text-gray mb-26">
-              Ashmita Vinyls Private Limited shall not be liable for any direct, indirect, incidental, special, or consequential damages resulting from the use or the inability to use our products, or for the cost of procurement of substitute goods.
+              Without prior approval and written permission, you may not create frames around our Webpages that alter in any way the visual presentation or appearance of our Website.
             </p>
 
-            <h3 className="title-text text-dark font-600 mb-10">6. Governing Law</h3>
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">Content Liability</h3>
             <p className="para-text text-gray mb-26">
-              These terms and conditions are governed by and construed in accordance with the laws of India. Any disputes arising in connection with these terms shall be subject to the exclusive jurisdiction of the courts in Mumbai, Maharashtra.
+              We shall not be held responsible for any content that appears on your Website. You agree to protect and defend us against all claims that are rising on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of, any third party rights.
             </p>
 
-            <h3 className="title-text text-dark font-600 mb-10">7. Contact Information</h3>
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">Your Privacy</h3>
             <p className="para-text text-gray mb-26">
-              For any questions or clarifications regarding our Terms & Conditions, please contact us at:<br />
-              <strong>Ashmita Vinyls Private Limited</strong><br />
-              Address: 3rd Floor, C-304, Primus Neptune Eastern Business District, Lal Bahadur Shastri Marg, Bhandup West, Mumbai, Maharashtra 400078<br />
-              Email: info@ashmitavinyls.com<br />
-              Phone: +91 85117 00544
+              Please read Privacy Policy.
+            </p>
+
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">Reservation of Rights</h3>
+            <p className="para-text text-gray mb-26">
+              We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amend these terms and conditions and it’s linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.
+            </p>
+
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">Removal of links from our website</h3>
+            <p className="para-text text-gray mb-26">
+              If you find any link on our Website that is offensive for any reason, you are free to contact and inform us any moment. We will consider requests to remove links but we are not obligated to do so or to respond to you directly.
+            </p>
+            <p className="para-text text-gray mb-26">
+              We do not ensure that the information on this website is correct, we do not warrant its completeness or accuracy; nor do we promise to ensure that the website remains available or that the material on the website is kept up to date.
+            </p>
+
+            <h3 className="title-text text-dark font-600 mb-10 mt-30">Disclaimer</h3>
+            <p className="para-text text-gray mb-10">
+              To the maximum extent permitted by applicable law, we exclude all representations, warranties and conditions relating to our website and the use of this website. Nothing in this disclaimer will:
+            </p>
+            <ul className="list-disc pl-20 mb-26">
+              <li className="para-text text-gray mb-8">limit or exclude our or your liability for death or personal injury;</li>
+              <li className="para-text text-gray mb-8">limit or exclude our or your liability for fraud or fraudulent misrepresentation;</li>
+              <li className="para-text text-gray mb-8">limit any of our or your liabilities in any way that is not permitted under applicable law; or</li>
+              <li className="para-text text-gray mb-8">exclude any of our or your liabilities that may not be excluded under applicable law.</li>
+            </ul>
+            <p className="para-text text-gray mb-26">
+              The limitations and prohibitions of liability set in this Section and elsewhere in this disclaimer: (a) are subject to the preceding paragraph; and (b) govern all liabilities arising under the disclaimer, including liabilities arising in contract, in tort and for breach of statutory duty.
+            </p>
+            <p className="para-text text-gray mb-26">
+              As long as the website and the information and services on the website are provided free of charge, we will not be liable for any loss or damage of any nature.
             </p>
           </div>
         </div>
